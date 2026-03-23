d0af8f2a5f5db08d0ac5bc16d87f9638c505476c
EneriCdl <807800242@qq.com>
2026-03-24 00:11:08 +0800

    feat: initial personal homepage

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿const seq = [
+  '.eyebrow',
+  'h1',
+  '.lead',
+  '.actions',
+  '.card:nth-child(1)',
+  '.card:nth-child(2)',
+  '.card:nth-child(3)'
+];
+
+seq.forEach((selector, i) => {
+  const el = document.querySelector(selector);
+  if (!el) return;
+  setTimeout(() => {
+    el.classList.add('reveal');
+  }, i * 120);
+});
